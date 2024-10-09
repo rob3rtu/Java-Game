397d9fa3fdd9c3d93629d87c3c5102e0ac61832e
Robert <robert.udrea@cst.ro>
2024-10-09 12:26:40 +0300

    game loop + moving character

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -12,12 +12,21 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    int FPS = 60;
+
+    KeyHandler keyHandler = new KeyHandler();
     Thread gameThread;
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
          this.setPreferredSize(new Dimension(screenWidth, screenHeight));
          this.setBackground(Color.black);
          this.setDoubleBuffered(true);
+         this.addKeyListener(keyHandler);
+         this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -28,5 +37,72 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
 
+        double drawInterval = 1000000000/FPS;
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+
+        while(gameThread != null) {
+            // 1 - Update information, like position for example
+            update();
+
+            // 2 - Draw the updated screen
+            repaint(); // this actually calls paintComponent()
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime /= 1000000;
+
+                if(remainingTime < 0) remainingTime = 0;
+
+                Thread.sleep((long)remainingTime);
+
+                nextDrawTime += drawInterval;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void update() {
+        if(keyHandler.upPressed) {
+            this.playerY -= this.playerSpeed;
+        } else  if(keyHandler.downPressed) {
+            this.playerY += this.playerSpeed;
+        } else if(keyHandler.leftPressed) {
+            this.playerX -= this.playerSpeed;
+        } else if(keyHandler.rightPressed) {
+            this.playerX += this.playerSpeed;
+        }
+    }
+
+    public void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+
+        Graphics2D graphics2D = (Graphics2D) graphics;
+        graphics2D.setColor(Color.white);
+        graphics2D.fillRect(this.playerX, this.playerY, this.tileSize, this.tileSize);
+
+        graphics2D.dispose();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
 
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setVisible(true);
         window.setResizable(false);
         window.setLocationRelativeTo(null);
         window.setTitle("Java 2D Game");
@@ -13,5 +12,9 @@ public class Main {
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
         window.pack();
+
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
     }
 }
